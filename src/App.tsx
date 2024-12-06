b98f3692e4526681fbcadf3ec8671131bb7630b1
JP Morgan <679434+npgenx@users.noreply.github.com>
2024-12-05 18:20:21 -0800

    added tailwinds AND scss

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+// import './custom.scss'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -25,7 +26,7 @@ function App() {
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
+      <p className="read-the-docs ">
         Click on the Vite and React logos to learn more
       </p>
     </>
